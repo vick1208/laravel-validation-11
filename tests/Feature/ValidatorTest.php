7e02dc22fe05eea6688a761b40d18bcd21b6da4c
Vicky AS <vickyhuang808@gmail.com>
2024-03-19 14:59:37 +0700

    validation message

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -81,8 +82,11 @@ class ValidatorTest extends TestCase
     }
     public function testValidationRules()
     {
+
+        App::setLocale('id');
+
         $data = [
-            "username" => "eko@mail.com",
+            "username" => "ek",
             "password" => "ek",
         ];
         $rules = [
@@ -120,11 +124,40 @@ class ValidatorTest extends TestCase
 
         try {
             $valid = $validator->validate();
-            Log::info(json_encode($valid,JSON_PRETTY_PRINT));
+            Log::info(json_encode($valid, JSON_PRETTY_PRINT));
         } catch (ValidationException $exception) {
             assertNotNull($exception->validator);
             $message = $exception->validator->errors();
             Log::error($message->toJson(JSON_PRETTY_PRINT));
         }
+    }
+
+    public function testValidationInlineMessage()
+    {
+        $data = [
+            "username" => "ek",
+            "password" => "ek",
+        ];
+        $rules = [
+            "username" => "required|email|max:100",
+            "password" => ["required", "min:6", "max:20"],
+        ];
+
+        $messages = [
+            "required" => ":attribute wajib diisi",
+            "email" => ":attribute wajib berupa email",
+            "min" => ":attribute minimal :min karakter",
+            "max" => ":attribute maksimal :max karakter",
+        ];
+
+        $validator = Validator::make($data, $rules, $messages);
+
+        assertNotNull($validator);
+
+        assertTrue($validator->fails());
+        assertFalse($validator->passes());
+
+        $message = $validator->getMessageBag();
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
     }
 }
